0867b3690a65e1bbed1011ffbbe9b8842c102ed9
Cristian Mazilu <mazilu6@gmail.com>
2023-06-10 23:26:28 +0300

    First commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace RollChannelControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBox1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 12);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(964, 527);
+            this.cartesianChart1.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(736, 571);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 545);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 642);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.Text = "RollChannelControl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Windows.Forms;
+
+namespace RollChannelControl
+{
+    public partial class Form1 : Form
+    {
+        private readonly ChartValues<double> _chartValues;
+        private readonly Timer _timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // initialize chart
+            cartesianChart1.Series = new LiveCharts.SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = _chartValues = new ChartValues<double>(),
+                }
+            };
+
+            // setup timer
+            _timer = new Timer { Interval = 50 };
+            _timer.Tick += TimerOnTick;
+
+            // start timer
+            _timer.Start();
+        }
+
+        private void TimerOnTick(object sender, EventArgs eventArgs)
+        {
+            // add new value and remove oldest if necessary
+            _chartValues.Add(System.Environment.TickCount);
+            if (_chartValues.Count > 50)
+                _chartValues.RemoveAt(0);
+
+            // refresh chart
+            cartesianChart1.Update(false, true);
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out var value))
+            {
+                // add value and start timer if necessary
+                _chartValues.Add(value);
+                if (!_timer.Enabled)
+                    _timer.Start();
+
+                // clear text box
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+}
